591c5012fc37cceaa7016a5f7e5b50df4355b056
Matt Arnold <mpa29@cam.ac.uk>
2016-11-22 21:21:53 +0000

    moving in form stuff

--- Part2Project/Form1.Designer.cs
+++ Part2Project/Form1.Designer.cs
@@ -28,21 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dlgChooseImage = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSigma = new System.Windows.Forms.TextBox();
+            this.txtK = new System.Windows.Forms.TextBox();
+            this.btnGBIS = new System.Windows.Forms.Button();
+            this.viewer2 = new System.Windows.Forms.PictureBox();
+            this.viewer = new System.Windows.Forms.PictureBox();
+            this.btnChooseImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.viewer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewer)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dlgChooseImage
+            // 
+            this.dlgChooseImage.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(271, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "sigma:";
+            this.label2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "k:";
+            this.label1.Visible = false;
+            // 
+            // txtSigma
+            // 
+            this.txtSigma.Location = new System.Drawing.Point(308, 21);
+            this.txtSigma.Name = "txtSigma";
+            this.txtSigma.Size = new System.Drawing.Size(53, 20);
+            this.txtSigma.TabIndex = 21;
+            this.txtSigma.Text = "0.0";
+            this.txtSigma.Visible = false;
+            // 
+            // txtK
+            // 
+            this.txtK.Location = new System.Drawing.Point(212, 21);
+            this.txtK.Name = "txtK";
+            this.txtK.Size = new System.Drawing.Size(53, 20);
+            this.txtK.TabIndex = 20;
+            this.txtK.Text = "400";
+            this.txtK.Visible = false;
+            // 
+            // btnGBIS
+            // 
+            this.btnGBIS.Location = new System.Drawing.Point(104, 11);
+            this.btnGBIS.Name = "btnGBIS";
+            this.btnGBIS.Size = new System.Drawing.Size(86, 37);
+            this.btnGBIS.TabIndex = 19;
+            this.btnGBIS.Text = "Segment Image";
+            this.btnGBIS.UseVisualStyleBackColor = true;
+            this.btnGBIS.Visible = false;
+            // 
+            // viewer2
+            // 
+            this.viewer2.Location = new System.Drawing.Point(338, 53);
+            this.viewer2.Name = "viewer2";
+            this.viewer2.Size = new System.Drawing.Size(320, 240);
+            this.viewer2.TabIndex = 18;
+            this.viewer2.TabStop = false;
+            // 
+            // viewer
+            // 
+            this.viewer.Location = new System.Drawing.Point(12, 53);
+            this.viewer.Name = "viewer";
+            this.viewer.Size = new System.Drawing.Size(320, 240);
+            this.viewer.TabIndex = 17;
+            this.viewer.TabStop = false;
+            // 
+            // btnChooseImage
+            // 
+            this.btnChooseImage.Location = new System.Drawing.Point(12, 12);
+            this.btnChooseImage.Name = "btnChooseImage";
+            this.btnChooseImage.Size = new System.Drawing.Size(86, 37);
+            this.btnChooseImage.TabIndex = 16;
+            this.btnChooseImage.Text = "Choose Image";
+            this.btnChooseImage.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(684, 311);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSigma);
+            this.Controls.Add(this.txtK);
+            this.Controls.Add(this.btnGBIS);
+            this.Controls.Add(this.viewer2);
+            this.Controls.Add(this.viewer);
+            this.Controls.Add(this.btnChooseImage);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.viewer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewer)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog dlgChooseImage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSigma;
+        private System.Windows.Forms.TextBox txtK;
+        private System.Windows.Forms.Button btnGBIS;
+        private System.Windows.Forms.PictureBox viewer2;
+        private System.Windows.Forms.PictureBox viewer;
+        private System.Windows.Forms.Button btnChooseImage;
     }
 }
 

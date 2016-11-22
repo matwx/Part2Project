10226fe5a079a23f9b82275de10d5ea63012ab18
Matt Arnold <mpa29@cam.ac.uk>
2016-11-22 17:36:17 +0000

    Removed some code smells from GBIS class

--- Part2Project/ImageSegmentation/GraphBasedImageSegmentation.cs
+++ Part2Project/ImageSegmentation/GraphBasedImageSegmentation.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
 using Kaliko.ImageLibrary;
 using Kaliko.ImageLibrary.Filters;
@@ -31,9 +28,9 @@ namespace Part2Project.ImageSegmentation
             return Math.Sqrt((lab1.A - lab2.A) * (lab1.A - lab2.A) + (lab1.B - lab2.B) * (lab1.B - lab2.B) + (lab1.L - lab2.L) * (lab1.L - lab2.L));
         }
 
-        private static void InitialiseEdges(Bitmap image, GraphBasedDisjointSet S, List<GraphEdge> E)
+        private static void InitialiseEdges(Bitmap image, GraphBasedDisjointSet dSet, List<GraphEdge> eList)
         {
-            GraphNode[][] V = S.getV();
+            GraphNode[][] v = dSet.getV();
 
             // Create all the edges for an 8-connected grid graph
             for (int x = 0; x < image.Width; x++)
@@ -51,25 +48,25 @@ namespace Part2Project.ImageSegmentation
                     if (x > 0)
                     {
                         // Left
-                        E.Add(new GraphEdge(V[x][y], V[x - 1][y], ComputeEdgeWeight(image, x, y, x - 1, y)));
+                        eList.Add(new GraphEdge(v[x][y], v[x - 1][y], ComputeEdgeWeight(image, x, y, x - 1, y)));
                     }
 
-                    if (y > 0)
+                    if (y <= 0) continue;
+                    // So, y > 0
+
+                    // Up
+                    eList.Add(new GraphEdge(v[x][y], v[x][y - 1], ComputeEdgeWeight(image, x, y, x, y - 1)));
+
+                    if (x > 0)
                     {
-                        // Up
-                        E.Add(new GraphEdge(V[x][y], V[x][y - 1], ComputeEdgeWeight(image, x, y, x, y - 1)));
+                        // Up-Left
+                        eList.Add(new GraphEdge(v[x][y], v[x - 1][y - 1], ComputeEdgeWeight(image, x, y, x - 1, y - 1)));
+                    }
 
-                        if (x > 0)
-                        {
-                            // Up-Left
-                            E.Add(new GraphEdge(V[x][y], V[x - 1][y - 1], ComputeEdgeWeight(image, x, y, x - 1, y - 1)));
-                        }
-
-                        if (x < image.Width - 1)
-                        {
-                            // Up-Right
-                            E.Add(new GraphEdge(V[x][y], V[x + 1][y - 1], ComputeEdgeWeight(image, x, y, x + 1, y - 1)));
-                        }
+                    if (x < image.Width - 1)
+                    {
+                        // Up-Right
+                        eList.Add(new GraphEdge(v[x][y], v[x + 1][y - 1], ComputeEdgeWeight(image, x, y, x + 1, y - 1)));
                     }
                 }
             }
@@ -88,44 +85,41 @@ namespace Part2Project.ImageSegmentation
 
         public static GraphBasedDisjointSet Segment(Bitmap image, double k, double sigma)
         {
-            List<GraphEdge> E;
-            GraphBasedDisjointSet S;
-
             // Transform the image as required
             image = ScaleAndBlur(image, sigma);
 
             // Set up Data Structures
-            S = new GraphBasedDisjointSet(image);
-            E = new List<GraphEdge>();
-            InitialiseEdges(image, S, E); // Create all the edges for an 8-connected grid graph
+            GraphBasedDisjointSet dSet = new GraphBasedDisjointSet(image);
+            List<GraphEdge> eList = new List<GraphEdge>();
+            InitialiseEdges(image, dSet, eList); // Create all the edges for an 8-connected grid graph
 
             // Sort E by non-decreasing edge weight
-            E.Sort();
+            eList.Sort();
 
-            // For each edge e
-            foreach (GraphEdge e in E)
+            // For each edge eList
+            foreach (GraphEdge e in eList)
             {
                 // If the edge joins two discinct components
-                if (S.FindSet(e.V1) != S.FindSet(e.V2))
+                if (dSet.FindSet(e.V1) != dSet.FindSet(e.V2))
                 { 
                     // If the weight is small enough compared to the components
                     // w <= MInt(C1, C2)
-                    if (e.Weight <= Math.Min(S.FindSet(e.V1).InternalDifference + k / S.FindSet(e.V1).ComponentSize,
-                                             S.FindSet(e.V2).InternalDifference + k / S.FindSet(e.V2).ComponentSize))
+                    if (e.Weight <= Math.Min(dSet.FindSet(e.V1).InternalDifference + k / dSet.FindSet(e.V1).ComponentSize,
+                                             dSet.FindSet(e.V2).InternalDifference + k / dSet.FindSet(e.V2).ComponentSize))
                     { 
                         // Then merge the two components
-                        S.Union(e.V1, e.V2, e.Weight);
+                        dSet.Union(e.V1, e.V2, e.Weight);
                     }
                 }
             }
 
-            return S;
+            return dSet;
         }
 
-        public static Bitmap VisualiseSegmentation(GraphBasedDisjointSet S) 
+        public static Bitmap VisualiseSegmentation(GraphBasedDisjointSet dSet) 
         {
-            int width = S.getV().Length;
-            int height = S.getV()[0].Length;
+            int width = dSet.getV().Length;
+            int height = dSet.getV()[0].Length;
 
             Random rand = new Random();
             Bitmap outputImage = new Bitmap(width, height);
@@ -136,19 +130,19 @@ namespace Part2Project.ImageSegmentation
             {
                 for (int y = 0; y < height; y++)
                 {
-                    if (!componentColours.ContainsKey(S.FindSetOfPixel(x,y)))
+                    if (!componentColours.ContainsKey(dSet.FindSetOfPixel(x,y)))
                     {
                         //componentColours.Add(FindSet(V[x][y]), Color.FromArgb(rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256)));
 
-                        CIELab labColor = new CIELab(S.FindSetOfPixel(x, y).TotalL / (double)S.FindSetOfPixel(x, y).ComponentSize,
-                                                     S.FindSetOfPixel(x, y).TotalA / (double)S.FindSetOfPixel(x, y).ComponentSize,
-                                                     S.FindSetOfPixel(x, y).TotalB / (double)S.FindSetOfPixel(x, y).ComponentSize);
+                        CIELab labColor = new CIELab(dSet.FindSetOfPixel(x, y).TotalL / dSet.FindSetOfPixel(x, y).ComponentSize,
+                                                     dSet.FindSetOfPixel(x, y).TotalA / dSet.FindSetOfPixel(x, y).ComponentSize,
+                                                     dSet.FindSetOfPixel(x, y).TotalB / dSet.FindSetOfPixel(x, y).ComponentSize);
                         RGB rgb = ColorSpaceHelper.LabtoRGB(labColor);
 
-                        componentColours.Add(S.FindSetOfPixel(x, y), Color.FromArgb(rgb.Red, rgb.Green, rgb.Blue));
+                        componentColours.Add(dSet.FindSetOfPixel(x, y), Color.FromArgb(rgb.Red, rgb.Green, rgb.Blue));
                     }
 
-                    outputImage.SetPixel(x, y, componentColours[S.FindSetOfPixel(x, y)]);
+                    outputImage.SetPixel(x, y, componentColours[dSet.FindSetOfPixel(x, y)]);
                 }
             }
 

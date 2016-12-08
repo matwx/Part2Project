aed788e0fd006bb11e7bd6faead911098de658b3
Matt Arnold <mpa29@cam.ac.uk>
2016-12-08 17:40:07 +0000

    Implemented Simplicity feature by thresholding the segment saliency map, and computing bounding boxes for the 'most salient' segments. The proportion of image area taken up by these boxes is the feature value.

---
+++ Part2Project/Features/FeatureSimplicity.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Part2Project.ImageSegmentation;
+
+namespace Part2Project.Features
+{
+    class FeatureSimplicity : IFeature
+    {
+        public double ComputeFeature(Bitmap image)
+        {
+            const double alpha = 0.67;
+
+            Segmentation s = GraphBasedImageSegmentation.Segment(image, 150.0, 0.8);
+            SaliencySegmentation ss = new SaliencySegmentation(s, image, 0.8);
+
+            bool[] trueSegments = new bool[ss.NumSegments];
+            int numTrueSegments = 0;
+
+            // Convert segment saliency map into a binary map, using a threshold, alpha
+            for (int i = 0; i < ss.NumSegments; i++)
+            {
+                if (ss.GetSegmentsSaliency(i) > alpha)
+                {
+                    trueSegments[i] = true;
+                    numTrueSegments++;
+                }
+                else trueSegments[i] = false;
+            }
+
+            int[] trueSegmentIndicies = new int[numTrueSegments];
+            int count = 0;
+            for (int i = 0; i < ss.NumSegments; i++)
+            {
+                if (trueSegments[i])
+                {
+                    trueSegmentIndicies[count] = i;
+                    count++;
+                }
+            }
+
+            // Generate bounding boxes for all of the segments that are 'true' in the binary ROI map
+            int[] lefts = new int[ss.NumSegments];
+            int[] rights = new int[ss.NumSegments];
+            int[] tops = new int[ss.NumSegments];
+            int[] bottoms = new int[ss.NumSegments];
+
+            for (int x = 0; x < ss.Width; x++)
+            {
+                for (int y = 0; y < ss.Height; y++)
+                {
+                    int i = ss.GetPixelsSegmentIndex(x, y);
+                    if (trueSegments[i])
+                    {
+                        if (x < lefts[i]) lefts[i] = x;
+                        if (x > rights[i]) rights[i] = x;
+                        if (y < tops[i]) tops[i] = y;
+                        if (y > bottoms[i]) bottoms[i] = y;
+                    }
+                }
+            }
+
+            bool[][] boundedBinarySaliencyMap = new bool[ss.Width][];
+            for (int x = 0; x < ss.Width; x++)
+            {
+                boundedBinarySaliencyMap[x] = new bool[ss.Height];
+            }
+
+            foreach (int i in trueSegmentIndicies)
+            {
+                // Add this segment's bounding box to the map
+                for (int x = lefts[i]; x < rights[i] + 1; x++)
+                {
+                    for (int y = tops[i]; y < bottoms[i] + 1; y++)
+                    {
+                        boundedBinarySaliencyMap[x][y] = true;
+                    }
+                }
+            }
+
+            // Feature value is the proportion of the image taken up by these bounding boxes.
+            int total = 0;
+            for (int x = 0; x < ss.Width; x++)
+            {
+                for (int y = 0; y < ss.Height; y++)
+                {
+                    if (boundedBinarySaliencyMap[x][y]) total++;
+                }
+            }
+
+            return ((double) total)/ss.Width/ss.Height;
+        }
+    }
+}

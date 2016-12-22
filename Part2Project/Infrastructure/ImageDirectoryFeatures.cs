473af1ea8a80bccdb942f505171100d9e98b1f01
Matt Arnold <mpa29@cam.ac.uk>
2016-12-22 15:56:42 +0000

    Need to commit to check other branch

--- Part2Project/Infrastructure/ImageDirectoryFeatures.cs
+++ Part2Project/Infrastructure/ImageDirectoryFeatures.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,60 @@ namespace Part2Project.Infrastructure
         // using Threadpool, and gather together the list of features for each image in the 
         // directory.
         private string _dPath;
+        private List<string> _imageFilenames;
+        private List<List<double>> _imageFeatures;
 
         public ImageDirectoryFeatures(string dPath)
         {
             _dPath = dPath;
+
+            // Get list of image filenames in the directory
+            _imageFilenames = new List<string>();
+            string[] filenames = Directory.GetFiles(_dPath);
+            foreach (string filename in filenames)
+            {
+                string ext = filename.Split('.').Last();
+                if (ext.Equals("jpg") || ext.Equals("jpeg") || ext.Equals("png"))
+                {
+                    // We'll accept these file extensions as images
+                    _imageFilenames.Add(filename);
+                }
+            }
         }
     }
 }
+
+//            dlgChooseFolder.ShowDialog();
+//            Dictionary<double, string> newNames = new Dictionary<double, string>();
+//
+//            var files = Directory.GetFiles(dlgChooseFolder.SelectedPath);
+//            foreach (string filename in files)
+//            {
+//                using (Image selected = Image.FromFile(filename))
+//                {
+//                    bmp = new Bitmap((int)((double)selected.Width / (double)selected.Height * (double)viewer.Height), viewer.Height);
+//                    Graphics gfx = Graphics.FromImage(bmp);
+//
+//                    gfx.DrawImage(selected, 0, 0, (int)((double)selected.Width / (double)selected.Height * (double)viewer.Height), viewer.Height);
+//
+//                    double value = new FeatureIntensityContrast().ComputeFeature(bmp);
+//
+//                    //newNames.Add(filename, dlgChooseFolder.SelectedPath + "\\" + value.ToString() + ".jpg");
+//                    newNames.Add(value, filename);
+//                }
+//
+//            }
+//
+//            List<double> keyList = new List<double>();
+//            foreach (double key in newNames.Keys)
+//            {
+//                keyList.Add(key);
+//            }
+//            keyList.Sort();
+//            keyList.Reverse();
+//            int current = 0;
+//            foreach (double key in keyList)
+//            {
+//                File.Move(newNames[key], dlgChooseFolder.SelectedPath + "\\" + current.ToString() + "--" + key.ToString() + "--IC.jpg");
+//                current++;
+//            }
--- Part2Project/Infrastructure/ImageFeatures.cs
+++ Part2Project/Infrastructure/ImageFeatures.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,24 @@ namespace Part2Project.Infrastructure
     class ImageFeatures
     {
         // Retrieves/computes a list of all features for a particular image
+        private Bitmap _image;
+
+        public ImageFeatures(string filename)
+        {
+            // Read the image as a bitmap
+            Image selected = Image.FromFile(filename);
+            _image = new Bitmap((int)((double)selected.Width / (double)selected.Height * 240.0), 240);
+            Graphics gfx = Graphics.FromImage(_image);
+            gfx.DrawImage(selected, 0, 0, (int)((double)selected.Width / (double)selected.Height * 240.0), 240);
+        }
+
+        List<double> GetFeatures()
+        {
+            List<double> result = new List<double>();
+
+
+
+            return result;
+        }
     }
 }

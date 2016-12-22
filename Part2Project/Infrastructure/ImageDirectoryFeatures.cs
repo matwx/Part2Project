bc440c4570e5a053ed7ca4713c4f3b274286ed78
Matt Arnold <mpa29@cam.ac.uk>
2016-12-22 15:33:02 +0000

    Set up classes to implement

---
+++ Part2Project/Infrastructure/ImageDirectoryFeatures.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2Project.Infrastructure
+{
+    class ImageDirectoryFeatures
+    {
+        // Given a directory path, configure and launch ImageFeatures threads for each image
+        // using Threadpool, and gather together the list of features for each image in the 
+        // directory.
+        private string _dPath;
+
+        public ImageDirectoryFeatures(string dPath)
+        {
+            _dPath = dPath;
+        }
+    }
+}
---
+++ Part2Project/Infrastructure/ImageFeatures.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2Project.Infrastructure
+{
+    class ImageFeatures
+    {
+        // Retrieves/computes a list of all features for a particular image
+    }
+}

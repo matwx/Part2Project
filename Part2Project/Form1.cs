f94f54b5a58cb04a060d448e545e540e8df0b157
Matt Arnold <mpa29@cam.ac.uk>
2017-01-10 16:40:37 +0000

    made RoT renaming segmentation sweep

--- Part2Project/Form1.cs
+++ Part2Project/Form1.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
 using Kaliko.ImageLibrary.ColorSpace;
+using Part2Project.Features;
 using Part2Project.ImageSegmentation;
 using Part2Project.Infrastructure;
 
@@ -101,7 +102,6 @@ namespace Part2Project
 
         private void DoSweep()
         {
-
             DateTime totalBefore = DateTime.Now;
             DateTime before, after;
             string dName =
@@ -193,22 +193,129 @@ namespace Part2Project
             }
         }
 
-        private void RoTRenameFromSegmentations()
+        private void RoTRenameFolder(string dName, string[] filenames, DirectBitmap[] originals, int k, double sigma)
         {
-            string dName =
-                "D:\\Users\\Matt\\Documents\\1 - Part II Project Tests\\Parameter Sweeps\\Segmentation k and sigma";
+            Task<double>[] tasks = new Task<double>[originals.Length];
 
-            string[] directories = Directory.GetDirectories(dName);
+            string dExt = "\\k" + k + "sigma" + sigma;
+            Directory.CreateDirectory(dName + dExt);
 
-            for (int i = 0; i < directories.Length - 1; i++) // Not including the last folder, which should be the originals one
+            for (int i = 0; i < originals.Length; i++)
             {
-                
+                var i1 = i;
+                tasks[i1] = Task<double>.Factory.StartNew(() =>
+                {
+                    Segmentation s = GraphBasedImageSegmentation.Segment(originals[i1], k, sigma);
+                    double result = FeatureRuleOfThirds.ComputeFeature(originals[i1], s);
+                    return result;
+                });
             }
+            Task<double[]> resultsTask = Task.WhenAll(tasks);
+            resultsTask.Wait();
+
+            // Sort and save all of images in the right folder, named by the RoT value
+            Dictionary<double, string> newNames = new Dictionary<double, string>();
+            for (int i = 0; i < filenames.Length; i++)
+            {
+                newNames.Add(resultsTask.Result[i], filenames[i]);
+            }
+            List<double> keyList = new List<double>();
+            foreach (double key in newNames.Keys)
+            {
+                keyList.Add(key);
+            }
+            keyList.Sort();
+            keyList.Reverse();
+            int current = 0;
+            foreach (double key in keyList)
+            {
+                originals[Array.IndexOf(filenames, newNames[key])].Bitmap.Save(dName + dExt + "\\" + current + "--" +
+                                                                               key + "." +
+                                                                               newNames[key].Split('.').Last());
+                current++;
+            }
+
+            foreach (Task<double> task in tasks)
+            {
+                task.Dispose();
+            }
+            resultsTask.Dispose();
+        }
+
+        private void RoTRenameSegmentationSweep()
+        {
+            DateTime totalBefore = DateTime.Now;
+            DateTime before, after;
+            string dName =
+                "D:\\Users\\Matt\\Documents\\1 - Part II Project Tests\\Parameter Sweeps\\Segmentation k and sigma - RoT";
+//            int[] kValues = { 25, 50, 75, 100, 125, 150, 175, 200, 250, 300, 400 };
+            int[] kValues = { 125, 150, 175, 200, 250, 300, 400 };
+            double[] sigmaValues = { 0.0, 0.4, 0.6, 0.8, 1.0, 1.4, 1.8, 2.5, 3.0, 4.0, 5.0 };
+
+            box.Text = "";
+            string nl = Environment.NewLine;
+
+            // Get the small images in memory
+            string[] fileNames = Directory.GetFiles(dName + "\\originals");
+            DirectBitmap[] originals = new DirectBitmap[fileNames.Length];
+
+            box.Text += "Segmentation k and sigma parameter sweep using RoT calculation" + nl + nl +
+                        "Loading original images" + nl;
+            for (int i = 0; i < originals.Length; i++)
+            {
+                // Load image from file, and shrink it down
+                using (Bitmap selected = new Bitmap(fileNames[i]))
+                {
+                    originals[i] = new DirectBitmap((int)((double)selected.Width / (double)selected.Height * 240.0), 240);
+                    using (Graphics gfx = Graphics.FromImage(originals[i].Bitmap))
+                    {
+                        gfx.DrawImage(selected, 0, 0, (int)((double)selected.Width / (double)selected.Height * 240.0),
+                            240);
+                    }
+                }
+            }
+
+            box.Text += "Images loaded" + nl;
+
+            int count = 0, k;
+            double sigma;
+            foreach (int kValue in kValues)
+            {
+                foreach (double sigmaValue in sigmaValues)
+                {
+                    k = kValue;
+                    sigma = sigmaValue;
+
+                    before = DateTime.Now;
+
+                    RoTRenameFolder(dName, fileNames, originals, k, sigma);
+
+                    GC.WaitForPendingFinalizers();
+                    GC.Collect();
+
+                    Thread.Sleep(2000);
+
+                    after = DateTime.Now;
+                    box.Text += "k=" + k + ", sigma=" + sigma + ": " + (after - before).TotalSeconds + " seconds to complete" + nl;
+                    Application.DoEvents();
+
+                    count++;
+                }
+            }
+
+            for (int i = 0; i < originals.Length; i++)
+            {
+                originals[i].Dispose();
+            }
+
+            DateTime totalAfter = DateTime.Now;
+
+            box.Text += nl + "In total took " + (totalAfter - totalBefore).TotalSeconds + " seconds to complete";
         }
 
         private void btnRoTRename_Click(object sender, EventArgs e)
         {
-            RoTRenameFromSegmentations();
+            RoTRenameSegmentationSweep();
         }
     }
 }

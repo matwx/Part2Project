9d7218ba17051d965ba8e92c65aeb5716cc66f4a
Matt Arnold <mpa29@cam.ac.uk>
2017-01-25 15:44:55 +0000

    Removed testing code

--- Part2Project/Form1.Designer.cs
+++ Part2Project/Form1.Designer.cs
@@ -28,70 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 58);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Image";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk_1);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 76);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(93, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 58);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Folder";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(660, 448);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(284, 261);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button2;
 
     }
 }
--- Part2Project/Form1.cs
+++ Part2Project/Form1.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Part2Project.Features;
-using Part2Project.ImageSegmentation;
-using Part2Project.Infrastructure;
 
 namespace Part2Project
 {
@@ -24,110 +13,6 @@ namespace Part2Project
         private void Form1_Load(object sender, EventArgs e)
         {
 
-        }
-
-        private void ConvexityRenameFolder(string dName)
-        {
-            // Get the small images in memory
-            string[] filenames = Directory.GetFiles(dName);
-            DirectBitmap[] originals = new DirectBitmap[filenames.Length];
-
-            for (int i = 0; i < originals.Length; i++)
-            {
-                // Load image from file, and shrink it down
-                using (Bitmap selected = new Bitmap(filenames[i]))
-                {
-                    originals[i] = new DirectBitmap((int)((double)selected.Width / (double)selected.Height * 240.0), 240);
-                    using (Graphics gfx = Graphics.FromImage(originals[i].Bitmap))
-                    {
-                        gfx.DrawImage(selected, 0, 0, (int)((double)selected.Width / (double)selected.Height * 240.0), 240);
-                    }
-                }
-            }
-
-            Task<double>[] tasks = new Task<double>[originals.Length];
-
-            for (int i = 0; i < originals.Length; i++)
-            {
-                var i1 = i;
-                tasks[i1] = Task<double>.Factory.StartNew(() =>
-                {
-                    const int k = 125;
-                    const double sigma = 0.6;
-                    Segmentation s = GraphBasedImageSegmentation.Segment(originals[i1], k, sigma);
-                    SaliencySegmentation ss = new SaliencySegmentation(s, originals[i1], sigma);
-
-                    double result = FeatureShapeConvexity.ComputeFeature(ss, originals[i1]);
-                    return result;
-                });
-            }
-            Task<double[]> resultsTask = Task.WhenAll(tasks);
-            resultsTask.Wait();
-
-            // Sort and save all of images in the right folder, named by the RoT value
-            Dictionary<double, string> newNames = new Dictionary<double, string>();
-            Random rand = new Random();
-            for (int i = 0; i < filenames.Length; i++)
-            {
-                if (newNames.ContainsKey(resultsTask.Result[i])) newNames.Add(resultsTask.Result[i] + rand.NextDouble() / 100000, filenames[i]);
-                else newNames.Add(resultsTask.Result[i], filenames[i]);
-            }
-            List<double> keyList = new List<double>();
-            foreach (double key in newNames.Keys)
-            {
-                keyList.Add(key);
-            }
-            keyList.Sort();
-            keyList.Reverse();
-            int current = 0;
-            foreach (double key in keyList)
-            {
-                File.Delete(newNames[key]);
-                originals[Array.IndexOf(filenames, newNames[key])].Bitmap.Save(dName + "\\" + current + "--" +
-                                                                               key + "." +
-                                                                               newNames[key].Split('.').Last());
-                current++;
-            }
-
-            foreach (Task<double> task in tasks)
-            {
-                task.Dispose();
-            }
-            resultsTask.Dispose();
-        }
-
-        private void openFileDialog1_FileOk_1(object sender, CancelEventArgs e)
-        {
-            using (Image selected = Image.FromFile(openFileDialog1.FileName))
-            {
-                DirectBitmap image = new DirectBitmap(
-                    (int) ((double) selected.Width / (double) selected.Height * 240.0), 240);
-                // Create the required resized images
-                using (Graphics gfx = Graphics.FromImage(image.Bitmap))
-                {
-                    gfx.DrawImage(selected, 0, 0, (int)((double)selected.Width / (double)selected.Height * 240.0), 240);
-                }
-
-                const int k = 125;
-                const double sigma = 0.6;
-                Segmentation s = GraphBasedImageSegmentation.Segment(image, k, sigma);
-                SaliencySegmentation ss = new SaliencySegmentation(s, image, sigma);
-                button1.Text = FeatureShapeConvexity.ComputeFeature(ss, image).ToString();
-
-                pictureBox1.Image = image.Bitmap;
-            }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            openFileDialog1.ShowDialog();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            folderBrowserDialog1.ShowDialog();
-
-            ConvexityRenameFolder(folderBrowserDialog1.SelectedPath);
         }
     }
 }

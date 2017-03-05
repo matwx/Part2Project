cb5f9acda0005f0b4d769a95b067cdae85959010
Matt Arnold <mpa29@cam.ac.uk>
2017-03-05 11:49:13 +0000

    Added blurring to generated edge map

--- Part2Project/Form1.Designer.cs
+++ Part2Project/Form1.Designer.cs
@@ -34,10 +34,26 @@
             this.dlgImage = new System.Windows.Forms.OpenFileDialog();
             this.viewer3 = new System.Windows.Forms.PictureBox();
             this.btnEdge = new System.Windows.Forms.Button();
-            this.btnEdge2 = new System.Windows.Forms.Button();
+            this.btnSelectRealEdges = new System.Windows.Forms.Button();
+            this.viewer4 = new System.Windows.Forms.PictureBox();
+            this.dlgEdges = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.viewer5 = new System.Windows.Forms.PictureBox();
+            this.viewer6 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.viewer1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.viewer2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.viewer3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewer4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewer5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewer6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // viewer1
@@ -89,22 +105,134 @@
             this.btnEdge.UseVisualStyleBackColor = true;
             this.btnEdge.Click += new System.EventHandler(this.btnEdge1_Click);
             // 
-            // btnEdge2
+            // btnSelectRealEdges
             // 
-            this.btnEdge2.Location = new System.Drawing.Point(174, 12);
-            this.btnEdge2.Name = "btnEdge2";
-            this.btnEdge2.Size = new System.Drawing.Size(75, 39);
-            this.btnEdge2.TabIndex = 5;
-            this.btnEdge2.Text = "Edge 2";
-            this.btnEdge2.UseVisualStyleBackColor = true;
-            this.btnEdge2.Click += new System.EventHandler(this.btnEdge2_Click);
+            this.btnSelectRealEdges.Location = new System.Drawing.Point(174, 12);
+            this.btnSelectRealEdges.Name = "btnSelectRealEdges";
+            this.btnSelectRealEdges.Size = new System.Drawing.Size(77, 39);
+            this.btnSelectRealEdges.TabIndex = 5;
+            this.btnSelectRealEdges.Text = "Select Real Edges";
+            this.btnSelectRealEdges.UseVisualStyleBackColor = true;
+            this.btnSelectRealEdges.Click += new System.EventHandler(this.btnSelectRealEdges_Click);
+            // 
+            // viewer4
+            // 
+            this.viewer4.Location = new System.Drawing.Point(12, 594);
+            this.viewer4.Name = "viewer4";
+            this.viewer4.Size = new System.Drawing.Size(320, 240);
+            this.viewer4.TabIndex = 6;
+            this.viewer4.TabStop = false;
+            // 
+            // dlgEdges
+            // 
+            this.dlgEdges.FileName = "openFileDialog1";
+            this.dlgEdges.FileOk += new System.ComponentModel.CancelEventHandler(this.dlgEdges_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 300);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Original";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(453, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Naive Edge Map";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(770, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Saliency Edge Map";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(138, 837);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "True Edges";
+            // 
+            // viewer5
+            // 
+            this.viewer5.Location = new System.Drawing.Point(338, 325);
+            this.viewer5.Name = "viewer5";
+            this.viewer5.Size = new System.Drawing.Size(320, 240);
+            this.viewer5.TabIndex = 11;
+            this.viewer5.TabStop = false;
+            // 
+            // viewer6
+            // 
+            this.viewer6.Location = new System.Drawing.Point(664, 325);
+            this.viewer6.Name = "viewer6";
+            this.viewer6.Size = new System.Drawing.Size(320, 240);
+            this.viewer6.TabIndex = 12;
+            this.viewer6.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(433, 568);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Blurred Naive Edge Map";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(758, 568);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Blurred Saliency Edge Map";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(433, 837);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(123, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Blurred Naive Edge Map";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(338, 594);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 587);
-            this.Controls.Add(this.btnEdge2);
+            this.ClientSize = new System.Drawing.Size(1000, 863);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.viewer6);
+            this.Controls.Add(this.viewer5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.viewer4);
+            this.Controls.Add(this.btnSelectRealEdges);
             this.Controls.Add(this.btnEdge);
             this.Controls.Add(this.viewer3);
             this.Controls.Add(this.btnSelectImage);
@@ -116,7 +244,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.viewer1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.viewer2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.viewer3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewer4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewer5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewer6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -128,7 +261,19 @@
         private System.Windows.Forms.OpenFileDialog dlgImage;
         private System.Windows.Forms.PictureBox viewer3;
         private System.Windows.Forms.Button btnEdge;
-        private System.Windows.Forms.Button btnEdge2;
+        private System.Windows.Forms.Button btnSelectRealEdges;
+        private System.Windows.Forms.PictureBox viewer4;
+        private System.Windows.Forms.OpenFileDialog dlgEdges;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox viewer5;
+        private System.Windows.Forms.PictureBox viewer6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
 
 
     }
--- Part2Project/Form1.cs
+++ Part2Project/Form1.cs
@@ -2,6 +2,8 @@
 using System.Drawing;
 using System.Security.Cryptography.X509Certificates;
 using System.Windows.Forms;
+using Kaliko.ImageLibrary;
+using Kaliko.ImageLibrary.Filters;
 using Part2Project.Features;
 using Part2Project.ImageSegmentation;
 using Part2Project.Infrastructure;
@@ -10,7 +12,7 @@ namespace Part2Project
 {
     public partial class Form1 : Form
     {
-        private DirectBitmap image;
+        private DirectBitmap image, edges;
 
         public Form1()
         {
@@ -33,11 +35,27 @@ namespace Part2Project
             {
                 using (Image selected = Image.FromFile(dlgImage.FileName))
                 {
-                    image = new DirectBitmap((int) ((double) selected.Width / (double) selected.Height * 240.0), 240);
+                    image = new DirectBitmap(320, 240);
                     // Create the required resized image
                     using (Graphics gfx = Graphics.FromImage(image.Bitmap))
                     {
-                        gfx.DrawImage(selected, 0, 0, (int)((double)selected.Width / (double)selected.Height * 240.0), 240);
+                        if (image.Width / (double)image.Height > 4.0 / 3.0)
+                        {
+                            // Too wide - crop left and right
+                            int originalWidth = (int)((double)image.Width / (double)image.Height * 240.0);
+                            gfx.DrawImage(selected, 160 - originalWidth / 2, 0, originalWidth, 240);
+                        }
+                        else if (image.Width / (double)image.Height < 4.0 / 3.0)
+                        {
+                            // Too narrow - crop top and bottom
+                            int originalHeight = (int)((double)image.Height / (double)image.Width * 320.0);
+                            gfx.DrawImage(selected, 0, 120 - originalHeight / 2, 320, originalHeight);
+                        }
+                        else
+                        {
+                            // Correct AR
+                            gfx.DrawImage(selected, 0, 0, 320, 240);
+                        }
                     }
 
                     
@@ -195,12 +213,71 @@ namespace Part2Project
 
         private void btnEdge1_Click(object sender, EventArgs e)
         {
-            viewer2.Image = Edge1().Bitmap;
-            viewer3.Image = Edge2().Bitmap;
+            DirectBitmap naiveEdges = Edge1();
+            DirectBitmap salEdges = Edge2();
+            DirectBitmap naiveEdges_B, salEdges_B;
+
+            viewer2.Image = naiveEdges.Bitmap;
+            viewer3.Image = salEdges.Bitmap;
+
+            float blurSigma = 1.2f;
+            using (KalikoImage kImage = new KalikoImage(naiveEdges.Bitmap))
+            {
+                kImage.ApplyFilter(new GaussianBlurFilter(blurSigma));
+                naiveEdges_B = new DirectBitmap(kImage.GetAsBitmap());
+            }
+            using (KalikoImage kImage = new KalikoImage(salEdges.Bitmap))
+            {
+                kImage.ApplyFilter(new GaussianBlurFilter(blurSigma));
+                salEdges_B = new DirectBitmap(kImage.GetAsBitmap());
+            }
+
+            viewer5.Image = naiveEdges_B.Bitmap;
+            viewer6.Image = salEdges_B.Bitmap;
         }
-        private void btnEdge2_Click(object sender, EventArgs e)
+
+        private void dlgEdges_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            
+            if (dlgEdges.FileName != "")
+            {
+                using (Image selected = Image.FromFile(dlgEdges.FileName))
+                {
+                    edges = new DirectBitmap(320, 240);
+                    // Create the required resized image
+                    using (Graphics gfx = Graphics.FromImage(edges.Bitmap))
+                    {
+                        if (edges.Width / (double)edges.Height > 4.0 / 3.0)
+                        {
+                            // Too wide - crop left and right
+                            int originalWidth = (int)((double)edges.Width / (double)edges.Height * 240.0);
+                            gfx.DrawImage(selected, 160 - originalWidth / 2, 0, originalWidth, 240);
+                        }
+                        else if (edges.Width / (double)edges.Height < 4.0 / 3.0)
+                        {
+                            // Too narrow - crop top and bottom
+                            int originalHeight = (int)((double)edges.Height / (double)edges.Width * 320.0);
+                            gfx.DrawImage(selected, 0, 120 - originalHeight / 2, 320, originalHeight);
+                        }
+                        else
+                        {
+                            // Correct AR
+                            gfx.DrawImage(selected, 0, 0, 320, 240);
+                        }
+                    }
+                }
+
+                viewer4.Image = edges.Bitmap;
+            }
+        }
+
+        private void btnSelectRealEdges_Click(object sender, EventArgs e)
+        {
+            dlgEdges.ShowDialog();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
